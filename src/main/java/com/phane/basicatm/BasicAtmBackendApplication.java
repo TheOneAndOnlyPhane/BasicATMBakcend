425b8dfc05850a7e38cb7754f8b7ddbc62b83614
TheOneAndOnlyPhane <cloud_2489@yahoo.com>
2020-11-02 08:47:09 +0200

    Created basic project structure with Spring Boot

---
+++ src/main/java/com/phane/basicatm/BasicAtmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phane.basicatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAtmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAtmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phane/basicatm/BasicAtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phane.basicatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
