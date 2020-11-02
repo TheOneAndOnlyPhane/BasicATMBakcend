d7449479f6c22595bad61da08b101b5f07506579
TheOneAndOnlyPhane <cloud_2489@yahoo.com>
2020-11-02 08:59:01 +0200

    "added h2 db configuration"

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO account(name, balance) VALUES ('Stefan', 30000);
+INSERT INTO account(name, balance) VALUES ('Andrei', 20000);
+INSERT INTO account(name, balance) VALUES ('Mihai', 15000);
+
+COMMIT;
